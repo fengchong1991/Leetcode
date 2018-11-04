f883bf867e5912732a088d04382ba947f0fd4f48
CF <fengchong1991@gmail.com>
2018-11-04 23:16:53 +1100

    no message

--- CrackTheCodingInterview/Chapter 4 Trees and Graphs/4.5.cs
+++ CrackTheCodingInterview/Chapter 4 Trees and Graphs/4.5.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CrackTheCodingInterview.Chapter_4_Trees_and_Graphs
 {
-    class _4_5
+    // Validate BST: Implement a function to check if a binary tree is a binary search tree.
+    public class _4_5
     {
+        // If the tree doesn't contain duplicates, traverse it with in-order traversal
+        public bool IsBinaryTree(BinaryTreeNode node)
+        {
+            var list = new List<int>();
+            Traversal(node, list);
+            
+            foreach(var i in list)
+            {
+                
+            }
+        }
+
+        public void Traversal(BinaryTreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Traversal(node.Left, list);
+            list.Add(node.Value);
+            Traversal(node.Right, list);
+        }
     }
 }
