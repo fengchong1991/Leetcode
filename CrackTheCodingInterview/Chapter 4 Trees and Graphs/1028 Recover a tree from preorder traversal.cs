340647e38ddb1f053016738f390df1dbe5fe2a39
Chong Feng <chong.feng@koresoftware.com>
2019-08-01 11:45:58 +1000

    -

---
+++ CrackTheCodingInterview/Chapter 4 Trees and Graphs/1028 Recover a tree from preorder traversal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackTheCodingInterview.Chapter_4_Trees_and_Graphs
+{
+    class _1028_Recover_a_tree_from_preorder_traversal
+    {
+        public TreeNode RecoverFromPreorder(string S)
+        {
+
+            var stack = new Stack<TreeNode>();
+
+            // Each level in the stack represents each level in the tree
+            if (stack.Count() == 0)
+            {
+
+            }
+
+
+    }
+    }
+}
---
+++ Leetcode/921 Minium Add to Make Parentheses Valid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class _921_Minium_Add_to_Make_Parentheses_Valid
+    {
+        // Running Time: O(N), N is the length of S
+        // Space O(1)
+        public int MinAddToMakeValid(string S)
+        {
+
+            var leftP = 0;
+            var rightP = 0;
+
+            foreach (var c in S)
+            {
+                if (c == '(')
+                {
+                    leftP++;
+                }
+                else
+                {
+                    if (leftP > 0)
+                    {
+                        leftP--;
+                    }
+                    else
+                    {
+                        rightP++;
+                    }
+                }
+            }
+
+            return leftP + rightP;
+        }
+    }
+}
