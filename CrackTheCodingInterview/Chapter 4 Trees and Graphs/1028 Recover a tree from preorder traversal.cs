b7abe56f1fc95e94a8e41394602bdef46f950ca3
Chong Feng <chong.feng@koresoftware.com>
2019-08-02 11:24:11 +1000

    -

--- CrackTheCodingInterview/Chapter 4 Trees and Graphs/1028 Recover a tree from preorder traversal.cs
+++ CrackTheCodingInterview/Chapter 4 Trees and Graphs/1028 Recover a tree from preorder traversal.cs
@@ -8,18 +8,64 @@ namespace CrackTheCodingInterview.Chapter_4_Trees_and_Graphs
 {
     class _1028_Recover_a_tree_from_preorder_traversal
     {
+        // Running time: Using Stack, O(S), S is the Length of S
+        // Space: O(N), N is the depth of the tree
         public TreeNode RecoverFromPreorder(string S)
         {
 
+            // Using Stack
             var stack = new Stack<TreeNode>();
 
-            // Each level in the stack represents each level in the tree
-            if (stack.Count() == 0)
+            for (int i = 0; i < S.Length;)
             {
+                var level = 0;
+                var number = 0;
+
+                while (i < S.Length && S[i] == '-')
+                {
+                    level++;
+                    i++;
+                }
+
+                while (i < S.Length && S[i] != '-')
+                {
+                    number = number * 10 + int.Parse(S[i].ToString());
+                    i++;
+                }
+
+                while (stack.Count > level)
+                {
+                    stack.Pop();
+                }
+
+                var newNode = new TreeNode(number);
+                if (stack.Count == 0)
+                {
+                    stack.Push(newNode);
+                }
+                else
+                {
+                    var parentNode = stack.Peek();
+                    if (parentNode.left == null)
+                    {
+                        parentNode.left = newNode;
+                    }
+                    else
+                    {
+                        parentNode.right = newNode;
+                    }
+
+                    stack.Push(newNode);
+                }
 
             }
 
+            while (stack.Count > 1)
+            {
+                stack.Pop();
+            }
 
-    }
+            return stack.Pop();
+        }
     }
 }
---
+++ Leetcode/979 Distribute Coins in Binary Tree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class _979_Distribute_Coins_in_Binary_Tree
+    {
+    }
+}
