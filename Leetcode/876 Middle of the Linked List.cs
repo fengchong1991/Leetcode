e5e7a052bc54c065d45ce275fd9d26af53968ef5
Chong Feng <chong.feng@koresoftware.com>
2019-08-05 16:57:20 +1000

    -

---
+++ Leetcode/876 Middle of the Linked List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class _876_Middle_of_the_Linked_List
+    {
+    }
+}
--- Leetcode/951 Flip Equvilant Binary Trees.cs
+++ Leetcode/951 Flip Equvilant Binary Trees.cs
@@ -8,6 +8,22 @@ namespace Leetcode
 {
     public class _951_Flip_Equvilant_Binary_Trees
     {
+        // Running time: O(N), N is the min number of nodes in two trees
+        // Space: O(H), H is the min height of two trees
+        public bool FlipEquiv(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return true;
+            }
 
+            if (root1 == null || root2 == null || root1.val != root2.val)
+            {
+                return false;
+            }
+
+            return (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right)) || (FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
+
+        }
     }
 }
