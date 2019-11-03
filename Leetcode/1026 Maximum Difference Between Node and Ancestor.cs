78b9a9f8e9861a1a9a043494a4e79d3a2e2190c2
Chong Feng <chong.feng@koresoftware.com>
2019-11-03 22:24:34 +1100

    -

--- Leetcode/1026 Maximum Difference Between Node and Ancestor.cs
+++ Leetcode/1026 Maximum Difference Between Node and Ancestor.cs
@@ -8,6 +8,33 @@ namespace Leetcode
 {
     class _1026_Maximum_Difference_Between_Node_and_Ancestor
     {
-        
+
+        /// <summary>
+        /// Running time: O(N), N is the number of nodes
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MaxAncestorDiff(TreeNode root)
+        {
+            return DFS(root, root.val, root.val);
+
+        }
+
+        public int DFS(TreeNode node, int min, int max)
+        {
+
+            if (node == null)
+            {
+                return max - min;
+            }
+
+            min = Math.Min(min, node.val);
+            max = Math.Max(max, node.val);
+
+            var left = DFS(node.left, min, max);
+            var right = DFS(node.right, min, max);
+
+            return Math.Max(left, right);
+        }
     }
 }
